4c670eb81c0d3cdc5cf7c83488eada9b8f946666
LucasCapSilva <57732522+LucasCapSilva@users.noreply.github.com>
2020-11-21 19:37:51 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    
+
+
+app = FastAPI()
+
+
+@app.get("/hello")
+async def create_item():
+
+    return {"mensagem":"hello word"}
