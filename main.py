4a1f03be63f68a1ddc9863875841621a00ca4689
unknown <generation@email.com>
2020-11-21 22:38:33 -0300

    analise sentimento

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
+  
 from typing import Optional
-
 from fastapi import FastAPI
 from pydantic import BaseModel
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+analyser = SentimentIntensityAnalyzer()
 
 class Item(BaseModel):
-    name: str
+    mensagem: str
     
-
-
 app = FastAPI()
-
 
 @app.get("/")
 async def create_item():
-
     return {"mensagem":"hello word"}
+
+@app.post("/analysy")
+async def create_item(item: Item):
+    score = analyser.polarity_scores(item.mensagem) # avaliação de polaridade de sentimento da mensagem
+    compound = (analyser.polarity_scores(item.mensagem)['compound'])  # capitura da média do sentimento da mensagem
+    if compound > 0:
+      mensagemSentimento = "mensagem positiva"
+    else:
+      mensagemSentimento = "mensagem negativa"
+    return {"mensagem":item.mensagem,"sentimento":mensagemSentimento}
